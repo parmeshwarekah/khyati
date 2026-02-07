1b500accf71255185d7f322ed75ade0eec10e056
Parmeshwar-ekah <parmeshwarekah@gmail.com>
2026-02-07 12:08:13 +0530

    changed page typsrpt

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,3 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import SearchBar from "@/components/SearchBar";
-
 export default function Home() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <SearchBar />
-    </>
-  );
+  return <h1>Khyati is live</h1>;
 }
