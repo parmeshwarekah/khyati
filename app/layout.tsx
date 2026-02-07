bd99326838e1b8b7a4c2858ffffc68e98fd428f5
Parmeshwar-ekah <parmeshwarekah@gmail.com>
2026-02-07 12:05:40 +0530

    added layout typsrpt

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Khyati",
+  description: "Universal Reputation Platform",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
